67ca228b975795ac57e3dee3a8dcccea798fe73b
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-10-03 18:51:56 +0200

    fix(header): eslint warnings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,14 @@ import ProfileIcon from '../assets/ProfileIcon'
 export default function Header() {
   return (
     <>
-      <header className="flex bg-[#111827] text-[#F9FAFB] items-center justify-between p-1">
+      <header className="flex items-center justify-between bg-[#111827] text-[#F9FAFB]  p-1">
         <button className="" onClick={() => {}}>
           Menu
         </button>
         <Logo className="relative z-10" />
         <ProfileIcon />
-        <div className="bg-[#111827] absolute h-20 w-20 rounded-full ml-[50%] -translate-x-[50%] z-1"></div>
+        {/* eslint-disable-next-line tailwindcss/enforces-negative-arbitrary-values */}
+        <div className="absolute z-1  h-20 w-20  rounded-full  bg-[#111827] ml-[50%] -translate-x-[50%] "></div>
       </header>
     </>
   )
