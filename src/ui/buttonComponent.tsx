9ea3b61f521dd6bdbe20869fea5ffbf36ee2725c
Preacher-Y <ysheja@gmail.com>
2025-10-03 15:37:37 +0200

    fix: added the button components prop for having all the attributes the button normally have

--- src/ui/buttonComponent.tsx
+++ src/ui/buttonComponent.tsx
@@ -1,16 +1,17 @@
-function ButtonComponent({
-  text,
-  className,
-  isPrimary,
-}: {
+import React from 'react'
+
+type ButtonComponentProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   text: string
-  className: string
   isPrimary: boolean
-}) {
+  className?: string
+}
+
+function ButtonComponent({ text, className, isPrimary, ...props }: ButtonComponentProps) {
   return (
     <button
-      className={`w-full rounded-full font-semibold ${className} 
-      ${isPrimary ? 'bg-gray-100 text-[#265E78]' : 'border-2 bg-[#265E78] text-white '} py-3`}
+      {...props}
+      className={`w-full rounded-full font-semibold  ${className ?? ''} 
+        ${isPrimary ? 'bg-gray-100 text-[#265E78]' : 'border-2 bg-[#265E78] text-white'} py-3`}
     >
       {text}
     </button>
