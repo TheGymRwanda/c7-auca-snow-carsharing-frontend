ef7bf7492ab7701eae13a02d58e6c9181709819e
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-10-03 18:46:53 +0200

    feat(header): creating component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,16 @@
+import Logo from '../assets/Logo'
+import ProfileIcon from '../assets/ProfileIcon'
 export default function Header() {
-  return <header>Header</header>
+  return (
+    <>
+      <header className="flex bg-[#111827] text-[#F9FAFB] items-center justify-between p-1">
+        <button className="" onClick={() => {}}>
+          Menu
+        </button>
+        <Logo className="relative z-10" />
+        <ProfileIcon />
+        <div className="bg-[#111827] absolute h-20 w-20 rounded-full ml-[50%] -translate-x-[50%] z-1"></div>
+      </header>
+    </>
+  )
 }
