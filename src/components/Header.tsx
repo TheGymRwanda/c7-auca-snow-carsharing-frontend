dfc2586c1c8b2537d030443a9ef932667c1fde15
Preacher-Y <ysheja@gmail.com>
2025-10-07 18:23:35 +0200

    fix: added left padding on the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
           Menu
         </button>
         <Link to={'/'}>
-          <Logo className="relative z-10" />
+          <Logo className="relative z-10 ml-4" />
         </Link>
         <ProfileIcon />
         {/* eslint-disable-next-line tailwindcss/enforces-negative-arbitrary-values */}
