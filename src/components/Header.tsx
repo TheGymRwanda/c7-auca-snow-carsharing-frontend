9867a88207420490d2cb6392769f792db170d458
Preacher-Y <ysheja@gmail.com>
2025-10-07 18:14:23 +0200

    fix: removed the small div i added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Logo from '../assets/Logo'
 import ProfileIcon from '../assets/ProfileIcon'
 export default function Header() {
@@ -7,8 +8,9 @@ export default function Header() {
         <button className="" onClick={() => {}}>
           Menu
         </button>
-        <div className="absolute w-16 h-14 left-[43.7%] top-[18px] rounded-full bg-[#111827]" />
-        <Logo className="relative mt-2 z-10" />
+        <Link to={'/'}>
+          <Logo className="relative z-10" />
+        </Link>
         <ProfileIcon />
         {/* eslint-disable-next-line tailwindcss/enforces-negative-arbitrary-values */}
         <div className="z-1 absolute ml-[50%] size-20 -translate-x-[50%] rounded-full bg-[#111827]"></div>
