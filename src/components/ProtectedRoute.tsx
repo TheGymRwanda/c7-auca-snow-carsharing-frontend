ce23600909f81b1d34702f4159aaae18ec9d2cc9
Preacher-Y <ysheja@gmail.com>
2025-10-08 15:00:05 +0200

    fix: added the type for react node

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../util/AuthContext'
+import type { ReactNode } from 'react'
 
 interface ProtectedRouteProps {
   children: ReactNode
