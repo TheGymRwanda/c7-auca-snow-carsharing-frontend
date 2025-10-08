5622c80fdbf9397be65288005f22af0daa1ece1d
Preacher-Y <ysheja@gmail.com>
2025-10-08 14:11:03 +0200

    fix(header): it was some how wiggling when the drop down is open and closed

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -8,17 +8,17 @@ import { Link } from 'react-router-dom'
 
 export default function DropDown() {
   return (
-    <nav className="absolute left-5 top-14 z-40 rounded-md bg-[#3E7591] p-4 text-sm text-white ">
+    <nav className="absolute left-5 top-20 z-40 rounded-md bg-[#3E7591] px-6 pb-2 pt-4 text-sm text-white ">
       <div>
         <ul>
           <Link to="/">
-            <li className="flex items-center gap-2 p-2">
+            <li className="flex items-center gap-2 p-2 hover:bg-[#51849d]">
               <CarIcon className="text-white" />
               Book A Car
             </li>
           </Link>
           <Link to="/bookings">
-            <li className="flex items-center gap-2 p-2">
+            <li className="flex items-center gap-2 p-2 hover:bg-[#51849d]">
               <MenuIcon />
               My Bookings
             </li>
@@ -28,26 +28,29 @@ export default function DropDown() {
         <p className="p-2 text-left font-semibold">My cars</p>
         <ul>
           <Link to="/car">
-            <li className="flex items-center gap-2 p-2">
+            <li className="flex items-center gap-2 p-2 hover:bg-[#51849d]">
               <CarsIcon />
               See My Cars
             </li>
           </Link>
           <Link to="/">
-            <li className="flex items-center gap-2 p-2">
+            <li className="flex items-center gap-2 p-2 hover:bg-[#51849d]">
               <ListIcon />
               My Cars Bookings
             </li>
           </Link>
           <Link to="/">
-            <li className="flex items-center gap-2 p-2 ">
+            <li className="flex items-center gap-2 p-2 hover:bg-[#51849d]">
               <CarPlusIcon />
               Add New Car
             </li>
           </Link>
         </ul>
         <hr />
-        <div className="flex items-center gap-2 p-2 py-4 " onClick={() => alert('Logging out...')}>
+        <div
+          className="flex items-center gap-2 p-2 hover:bg-[#51849d]"
+          onClick={() => alert('Logging out...')}
+        >
           <LogoutIcon />
           <p>Logout</p>
         </div>
